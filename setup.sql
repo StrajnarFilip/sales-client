84f071afa81884b6830e6ef030547ff29e2479c8
Filip Strajnar <filip.strajnar@gmail.com>
2023-04-28 22:00:10 +0200

    Initial project structure

---
+++ setup.sql
@@ -0,0 +1,76 @@
+DROP TABLE IF EXISTS prices;
+DROP TABLE IF EXISTS transactions;
+DROP TABLE IF EXISTS sales;
+DROP TABLE IF EXISTS items;
+
+CREATE TABLE items(
+  id BIGSERIAL primary key,
+  name TEXT unique,
+  time_added TIMESTAMP default NOW()
+);
+
+CREATE TABLE prices(
+  id BIGSERIAL primary key,
+  item bigint references items(id),
+  price numeric(16,4),
+  time_added TIMESTAMP default NOW()
+);
+
+CREATE TABLE sales(
+  id BIGSERIAL primary key,
+  user_id uuid,
+  time_added TIMESTAMP default NOW(),
+  sold boolean default false
+);
+
+CREATE TABLE transactions(
+  id BIGSERIAL primary key,
+  quantity numeric(16,4),
+  price numeric(16,4),
+  item bigint references items(id),
+  sale bigint references sales(id)
+);
+
+ALTER table items ENABLE ROW LEVEL SECURITY;
+ALTER table prices ENABLE ROW LEVEL SECURITY;
+ALTER table sales ENABLE ROW LEVEL SECURITY;
+ALTER table transactions ENABLE ROW LEVEL SECURITY;
+
+-- Everyone may view all items
+CREATE POLICY "Enable read access for all users" ON "public"."items"
+AS PERMISSIVE FOR SELECT
+TO public
+USING (true);
+
+-- User may only work on data with their own user id.
+CREATE POLICY "User insert sale with their own ID" ON "public"."sales"
+AS PERMISSIVE FOR ALL
+TO authenticated
+WITH CHECK (auth.uid() = user_id);
+
+-- User may only work on data with their own user id.
+CREATE POLICY "User insert sale with their own ID" ON "public"."transactions"
+AS PERMISSIVE FOR ALL
+TO authenticated
+WITH CHECK (auth.uid() = (SELECT user_id FROM sales WHERE sale = sales.id));
+
+-- This function gets latest price (based on timestamp) of item with item_id.
+CREATE OR REPLACE function latest_price(item_id bigint) returns setof numeric(16,4) as $$
+  SELECT price FROM prices
+  WHERE prices.id =
+  (SELECT max(mp.id) FROM
+  (SELECT * FROM prices as p WHERE item_id = p.item) as mp);
+$$ language sql;
+
+-- This function makes it convenient to add an item and the latest price for it.
+CREATE OR REPLACE function new_item(item_name text, initial_price numeric(16,4)) returns bigint as $$
+  insert into items (name) values (item_name);
+  insert into prices (item,price) values ((select id from items where items.name = item_name), initial_price);
+  select id from items where items.name = item_name;
+$$ language sql;
+
+select * from new_item('Cheese', 10);
+select * from new_item('Tomato', 1.99);
+select * from new_item('Potato', 0.69);
+select * from new_item('Corn', 4.8);
+select * from new_item('Oats', 1.18);
